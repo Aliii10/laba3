3020d226a1ce1a31c2ef4a5a3ba4775a2f3a1665
Aliii10 <127393702+Aliii10@users.noreply.github.com>
2023-05-15 21:54:22 +0300

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,29 @@
 package org.example;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        try(FileWriter writer = new FileWriter("comand.txt", false))
         int result1 = teamNumber1();
         int result2 = teamNumber2();
-        if (result1 > result2) {
+        if (result1 < result2) {
             System.out.println("Победила первая команда с результатом: " + result1);
-        } else if (result1 < result2) {
+        } else if (result1 > result2) {
             System.out.println("Победила вторая команда с результатом: " + result2);
         } else {
             System.out.println("Победила дружба!!! Результаты команд: " + result2);
         }
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     }
+    Scanner console = new Scanner(System.in);
+    System.out.print("Введите название первой команды: ");
+    Team team1 = new Team(console.nextLine());
+    System.out.print("Введите название второй команды: ");
+    Team team2 = new Team(console.nextLine());
 
+    int result1 = team1.sumTime();
+    int result2 = team2.sumTime();
     static int teamNumber1() {
         System.out.println("Список спортсменов первой команды: ");
         String text = """
@@ -28,16 +33,14 @@ public class Main {
                 4. Александров Александр Александрович
                 5. Снежкова Мария Юрьевна
                 """;
-        Writer.write(text);
         System.out.println(text);
-        System.out.println("ВВведите количество медалей каждого спортсмена первой команды: ");
+        System.out.println("Введите время каждого спортсмена первой команды: ");
 
-        int [] medals1 = inputMedals();
+        int [] time1 = inputTime();
 
-        int summa = summaMedals(medals1);
-        System.out.println("Сумма медального зачёта первой команды: " + summa);
+        int summa = sumTime(time1);
+        System.out.println("Сумма общего времени первой команды: " + summa);
         return summa;
-
     }
 
     static int teamNumber2() {
@@ -52,31 +55,21 @@ public class Main {
                 5. Владов Владлен Владиславович
                 """;
         System.out.println(text2);
-        System.out.println("Ввведите количество медалей каждого спортсмена второй команды: ");
+        System.out.println("Ввведите время каждого спортсмена второй команды: ");
 
-        int[] medals2 = inputMedals();
-
-        int summa = summaMedals(medals2);
-        System.out.println("Сумма медального зачёта второй команды: " + summa);
+        int[] time2 = inputTime();
+        int summa = sumTime(time2);
+        System.out.println("Сумма общего времени второй команды: " + summa);
         System.out.println();
         return summa;
     }
 
-    public static int[] inputMedals() {
+    public static int[] inputTime() {
         int[] medals = new int[5];
         Scanner console = new Scanner(System.in);
         for (int i = 0; i < medals.length; i++) {
             medals[i] = console.nextInt();
         }
         return medals;
-    }
-
-    public static int summaMedals(int[] array){
-        int result = 0;
-
-        for(int i = 0; i < array.length; i++){
-            result += array[i];
-        }
-        return result;
     }
 }
